1ffb1246c538f069bf9220e7648ff697d9f48c9e
David Emanuel Muresan <emanuel.muresan@emmanuel.ro>
2024-01-23 15:03:21 +0200

    Adunare

---
+++ Adunare/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace Adunarea a doua numere
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int x;
+            int y;
+            int z;
+            x = Convert.ToInt32(Console.ReadLine());
+            y = Convert.ToInt32(Console.ReadLine());
+
+            z = x + y;
+            Console.WriteLine(z);
+
+        }
+    }
+}
